e5aa68323a4db09bec8e242be1c3c308281574e3
tcheng9 <tcheng789@gmail.com>
2022-07-24 17:02:56 -0500

    update

---
+++ instagram-react-app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCw-GgFNQcF-Lh_JQ15lvMEJLzT4OGlISU",
+  authDomain: "instag-9e251.firebaseapp.com",
+  projectId: "instag-9e251",
+  storageBucket: "instag-9e251.appspot.com",
+  messagingSenderId: "579641322170",
+  appId: "1:579641322170:web:86e2e78b0139123c5cb083"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
