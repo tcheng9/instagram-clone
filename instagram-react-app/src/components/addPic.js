8ac83620e985f4bc2b157bdc7814ca5ced13b301
Tommy C <tcheng789@gmail.com>
2022-08-09 13:02:29 -0500

    update

--- instagram-react-app/src/components/addPic.js
+++ instagram-react-app/src/components/addPic.js
@@ -1,12 +1,39 @@
 import {Link} from 'react-router-dom';
+import {storage} from '../firebase';
+import {useState} from "react";
+import {ref, uploadBytes} from "firebase/storage";
 
 const AddPic = () => {
+
+    const [imageUpload, setImageUpload] = useState(null);
+
+    const uploadImage = () => {
+       
+      if (imageUpload == null) return;
+      console.log('clicked');
+      const imageRef = ref(storage);
+      uploadBytes(imageRef, imageUpload).then(() => {
+        alert("image uploaded");
+      })
+    }
+    const onSubmit = (e) => {
+      e.preventDefault();
+
+    }
+
     return (
       <div>
         <h1>Hello from add pic page </h1>
         
         <Link to = "/"> Main page </Link>
       
+      <div id = "picInputForm">
+        
+          <input type = "file" onChange = {(event) => {setImageUpload(event.target.files[0])}} />
+          <input type = "text" name = "username" placeholder = "NAME" />
+          <button onClick = {uploadImage}> Upload image </button>
+        
+      </div>
       </div>
     );
   };
--- instagram-react-app/src/firebase.js
+++ instagram-react-app/src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
-import 'firebase/storage';
+import {getStorage} from "firebase/storage"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,10 +17,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 
+export const storage = getStorage()
 const provider = new GoogleAuthProvider();
 
 export const signInWithGoogle = () => {
