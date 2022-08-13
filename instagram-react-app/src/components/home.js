b1955a09c14c945c7a2d062fbf99e289a734dea0
tcheng9 <tcheng789@gmail.com>
2022-08-12 22:20:16 -0500

    update

--- instagram-react-app/src/components/home.js
+++ instagram-react-app/src/components/home.js
@@ -1,15 +1,39 @@
 import {Link} from 'react-router-dom';
+import {useState, useEffect} from "react";
+import {storage} from "../firebase";
+import {ref, uploadBytes, listAll, getDownloadURL} from "firebase/storage";
 
 const HomePage = () => {
-    return (
-      <div>
-        <h1>Hello from Main/Home page of instagram</h1>
 
-        <ul>
-        <Link to = "/"> Main page </Link>
-      </ul>
-      </div>
-    );
-  };
+const [imageList, setImageList] = useState([]);
+
+const imageListRef = ref(storage, "images/");
+
+useEffect(() => {
+  listAll(imageListRef).then((response) => {
+    response.items.forEach((item) => {
+      getDownloadURL(item).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      })
+    })
+  });
+},[])
+
+  return (
+    <div>
+      <h1>Hello from Main/Home page of instagram</h1>
+
+      <ul>
+      <Link to = "/"> Main page </Link>
+    </ul>
+
+    <div id = "imgList">
+      {imageList.map((url) => {
+        return <img src = {url}/>
+      })}
+    </div>
+    </div>
+  );
+};
   
   export default HomePage;
