d02f8c6b1b9f1e052de747f0d60469d879e09e56
tcheng9 <tcheng789@gmail.com>
2022-08-23 19:35:05 -0500

    update

--- instagram-react-app/src/components/home.js
+++ instagram-react-app/src/components/home.js
@@ -2,23 +2,44 @@ import {Link} from 'react-router-dom';
 import {useState, useEffect} from "react";
 import {storage} from "../firebase";
 import {ref, uploadBytes, listAll, getDownloadURL} from "firebase/storage";
+import { firestoreDb } from '../firebase';
+import {getDocs, collection} from 'firebase/firestore';
 
 const HomePage = () => {
 
 const [imageList, setImageList] = useState([]);
+const [photos, setPhotos] =  useState([]);
 
 const imageListRef = ref(storage, "images/");
 
-useEffect(() => {
-  listAll(imageListRef).then((response) => {
-    response.items.forEach((item) => {
-      getDownloadURL(item).then((url) => {
-        setImageList((prev) => [...prev, url]);
-      
-      })
+//Refercing DB collection
+const colRef = collection(firestoreDb, 'posts');
+
+//Get collection's data
+
+getDocs(colRef)
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      photos.push({...doc.data(), id:doc.id});
     })
-  });
-}, [])
+    console.log(photos);
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+
+
+
+// useEffect(() => {
+//   listAll(imageListRef).then((response) => {
+//     response.items.forEach((item) => {
+//       getDownloadURL(item).then((url) => {
+//         setImageList((prev) => [...prev, url]);
+      
+//       })
+//     })
+//   });
+// }, [])
 
 
 // I think useEffect and setImageList is calling duplicates
